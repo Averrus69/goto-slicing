ef670f7302241a5d4667636928fac48bc7458e0c
Averrus69 <arryaverusai4@gmail.com>
2024-12-01 06:56:24 +0700

    update

---
+++ src/app/components/hero.tsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+  return (
+    <div className="h-[100vh] px-20 bg-black text-white">
+      <div className="h-full top-1/2 translate-y-1/2">
+        <h1 className="font-bold text-9xl">GoTo</h1>
+        <h2 className="font-semibold text-6xl">Mendorong Kemajuan</h2>
+      </div>
+    </div>
+  );
+}
